dcf7a811f771accfbfcac0c0168a8df815001b67
GabrielDevSpooky <gabrielicmusa@gmail.com>
2023-11-08 00:13:50 -0300

    Formatting App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
-<div class="min-h-screen">
-  <div class="pb-72">
-    <navBar></navBar>
-        <LayoutPage></LayoutPage>
-    <foot class="fixed bottom-0"></foot>
+  <div class="min-h-screen">
+    <div class="pb-72">
+      <navBar></navBar>
+      <LayoutPage></LayoutPage>
+      <foot class="fixed bottom-0"></foot>
+    </div>
   </div>
-</div>
 </template>
 <script>
-  import LayoutPage from './layout/LayoutPage.vue'
-  import navBar from './components/navBar.vue'
-  import foot from './components/foot.vue'
+import LayoutPage from "./layout/LayoutPage.vue";
+import navBar from "./components/navBar.vue";
+import foot from "./components/foot.vue";
 
-  export default {
-    components: {
-      LayoutPage,
-      navBar,
-      foot
-    }
-  }
+export default {
+  components: {
+    LayoutPage,
+    navBar,
+    foot,
+  },
+};
 </script>
