01f49c61eebaf291aaae076c737877b5d876fd9d
GabrielDevSpooky <gabrielicmusa@gmail.com>
2023-11-08 00:52:14 -0300

    Improved Dashboard styling and text

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen">
     <div class="flex mt-5">
       <v-select
-        class="ml-5 w-96 rounded-md bg-gray-200"
+        class="ml-5 h-full w-96 rounded-md bg-gray-200"
         id="base"
         :options="currencyList[quote]['pairs']"
         :clearable="false"
@@ -13,13 +13,13 @@
         class="ml-4 text-white focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-gray-800 hover:bg-gray-700 focus:ring-gray-700 border-gray-700"
         @click="addCoinPair"
       >
-        <i class="fa fa-plus fa-lg" aria-hidden="true">Add</i>
+        Add
       </button>
       <button
         class="ml-4 text-white focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-gray-800 hover:bg-gray-700 focus:ring-gray-700 border-gray-700"
         @click="clear"
       >
-        Clear
+        Reset
       </button>
     </div>
     <CryptoBoard></CryptoBoard>
