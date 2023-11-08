c885be79fdca1e5051842f30fc707eda52597769
GabrielDevSpooky <gabrielicmusa@gmail.com>
2023-11-08 01:08:01 -0300

    Formatting navBar.vue

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
-    <nav class="border-gray-200 px-2 sm:px-4 py-2.5  bg-gray-800">
-      <div class="container flex flex-wrap justify-between items-center mx-auto">
+    <nav class="border-gray-200 px-2 sm:px-4 py-2.5 bg-gray-800">
+      <div
+        class="container flex flex-wrap justify-between items-center mx-auto"
+      >
         <a class="flex items-center">
-          <img src="../images/logo.png" class="mr-3 h-6 sm:h-9" alt="CryptoMaster Logo" />
-          <span class="self-center text-xl font-semibold whitespace-nowrap text-white">CryptoMaster</span>
+          <img
+            src="../images/logo.png"
+            class="mr-3 h-6 sm:h-9"
+            alt="CryptoMaster Logo"
+          />
+          <span
+            class="self-center text-xl font-semibold whitespace-nowrap text-white"
+            >CryptoMaster</span
+          >
         </a>
       </div>
     </nav>
