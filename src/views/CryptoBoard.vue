06db02b02bf399c5362233bd650b9807d8ef770f
GabrielDevSpooky <gabrielicmusa@gmail.com>
2023-11-17 14:57:03 -0300

    CryptoBoard.vue formatting

--- src/views/CryptoBoard.vue
+++ src/views/CryptoBoard.vue
@@ -1,23 +1,26 @@
 <template>
-  <div class="grid gap-4 lg:grid-cols-3 lg:grid-rows-1 md:grid-cols-2 md:grid-rows-2 sm:grid sm:grid-cols-1 sm:grid-rows-3">
+  <div
+    class="grid gap-3 p-4 lg:grid-cols-3 lg:grid-rows-1 md:grid-cols-2 md:grid-rows-2 sm:grid sm:grid-cols-1 sm:grid-rows-3"
+  >
     <div v-for="value in currencies" :key="value.symbol">
-      <CurrencyCard :ticker="tickers[value.symbol] || {}" :info="value"></CurrencyCard>
+      <CurrencyCard
+        :ticker="tickers[value.symbol] || {}"
+        :info="value"
+      ></CurrencyCard>
     </div>
   </div>
 </template>
 
-
-
 <script>
-  import CurrencyCard from '../components/CurrencyCard.vue'
-  import { mapState } from 'vuex'
-  export default {
-    name: 'CryptoBoard',
-    components: {
-      CurrencyCard
-    },
-    computed: {
-      ...mapState(['tickers', 'currencies'])
-    }
-  }
+import CurrencyCard from "../components/CurrencyCard.vue";
+import { mapState } from "vuex";
+export default {
+  name: "CryptoBoard",
+  components: {
+    CurrencyCard,
+  },
+  computed: {
+    ...mapState(["tickers", "currencies"]),
+  },
+};
 </script>
